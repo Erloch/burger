af229e72ad2ebb2174a2134aa1bfc7dd937bdec0
Erich B <erichbernhardt@hotmail.com>
2019-07-15 09:44:40 -0600

    added some files still in pregress

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE  burgers_db;
+
+CREATE TABLE burgers (
+	item_id INTEGER AUTO_INCREMENT NOT NULL,
+
+    burger_name VARCHAR(100),
+
+    devoured BOOLEAN,
+    
+    PRIMARY KEY (item_id)
+);
+
+SELECT * FROM burgers_db;
